5bf6c59b3016078d89ada2fde3ac4e5729b07840
emmakbradley <emmabradley@g.ucla.edu>
2022-03-17 23:40:43 -0700

    added all files

---
+++ app.py
@@ -0,0 +1,187 @@
+'''
+# Local Preview
+At the command line: 
+
+conda activate PIC16B
+export FLASK_ENV=development; flask run
+
+# Sources:
+Lecture notes from class, and flask-examples repository from Professor Chodrow on GitHub
+'''
+
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import collections
+from plotly import express as px
+import numpy as np
+from wordsegment import load, segment
+
+import io
+import random
+
+import plotly
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from plotly import express as px
+
+from flask import Flask, render_template, request, g
+
+
+import numpy as np
+import re
+from flask import Flask, render_template
+
+
+import wordsegment
+from wordsegment import load, segment
+load()
+import nltk
+nltk.download('vader_lexicon')
+
+from tiktok_functions import make_scatterplot, clean_tiktok_df, get_sentiment, make_piechart
+
+
+app = Flask(__name__)
+
+def get_tiktok_db():
+    ''' 
+    A function that accesses the tiktok database through the Flask web app
+    '''
+    # connect to db
+    g.tiktok_db = sqlite3.connect("tiktok.db")
+
+    # return connection
+    return g.tiktok_db
+
+def app_scatterplot(num_days):
+    ''' 
+    A function that passes a scatter plot of the sentiment analysis scores to the view.html template
+    '''
+    #connect to database
+    g.tiktok_db = get_tiktok_db()
+    
+    cmd = \
+    f"""
+    SELECT id, video_title, upload_time, sound_transcribed, like, view, date_pulled
+    FROM tiktok 
+    """ 
+    tiktoks = pd.read_sql_query(cmd, g.tiktok_db)
+
+    # close database
+    g.tiktok_db.close()
+
+    # clean df
+    tiktoks = clean_tiktok_df(tiktoks)
+    tiktoks = tiktoks[tiktoks['rank'] <= num_days]
+
+    # get sentiments
+    tiktoks = get_sentiment(tiktoks)
+
+    # plot sentiments
+    fig = make_scatterplot(tiktoks)
+
+    return fig
+
+def app_piechart(num_days):
+    ''' 
+    A function that passes a pie chart of the sentiment analysis scores to the view.html template
+    '''
+    #connect to database
+    g.tiktok_db = get_tiktok_db()
+    
+    cmd = \
+    f"""
+    SELECT id, video_title, upload_time, sound_transcribed, like, view, date_pulled
+    FROM tiktok 
+    """ 
+    tiktoks = pd.read_sql_query(cmd, g.tiktok_db)
+
+    # close database
+    g.tiktok_db.close()
+
+    # clean df
+    tiktoks = clean_tiktok_df(tiktoks)
+    tiktoks = tiktoks[tiktoks['rank'] <= num_days]
+
+    # get sentiments
+    tiktoks = get_sentiment(tiktoks)
+
+    # plot sentiments
+    fig = make_piechart(tiktoks)
+
+    return fig
+
+
+@app.route("/")
+def main():
+    '''
+    The app route that is called when the web app is first initialized
+    '''
+    return render_template('test.html')
+    
+@app.route('/submit1/', methods=['GET', 'POST'])
+def submit1():
+    '''
+    The app route that is called when the user wants to view the sentiments
+    of trending TikToks. Allows the user to specify the format the data is
+    displayed in.
+    '''
+
+    if request.method == 'GET':
+        return render_template('submit1.html')
+    else:
+        # get num_days
+        num_days = request.form['num_days']
+        num_days = int(num_days)
+
+        if num_days > 13:
+            return render_template('submit1.html', error=True)
+    
+
+        # get plot type
+        plot = request.form['plot']
+
+        # scatter plot
+        if plot == 'scatter':
+            fig = app_scatterplot(num_days)
+            graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+        # pie chart
+        else: 
+            fig = app_piechart(num_days)
+            graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+            
+        return render_template('view1.html', graphJSON=graphJSON)
+
+
+@app.route('/view1/')
+def view1():
+    '''
+    The app route that is called so that the user can view the plot of sentiment scores.
+    '''
+    return render_template('view1.html', graphJSON=graphJSON)
+
+@app.route('/about_us/')
+def about_us():
+    '''
+    The app route that is called when the user clicks on the 'About Us' tab of the web app
+    '''
+    return render_template('about_us.html')
+
+@app.route('/what/')
+def what():
+    '''
+    The app route that is called when the user clicks on the 'Our Goal' tab of the web app
+    '''
+    return render_template('what.html')
+    
+
+    
+
+    
+
+
---
+++ tiktok_functions.py
@@ -0,0 +1,159 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+import collections
+from plotly import express as px
+import numpy as np
+from wordsegment import load, segment
+import re
+
+import wordsegment
+from wordsegment import load, segment
+load()
+
+import nltk
+nltk.download('vader_lexicon')
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import subjectivity
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+
+def clean_tiktok_df(tiktoks):
+    '''
+    A function for cleaning the raw dataframe of TikToks obtained from the unofficial TikTok API.
+    '''
+    
+    #fix column 'upload_time' -> 'video_url'
+    tiktoks = tiktoks.rename(columns = {'upload_time' : 'video_url'})
+    
+    #remove duplicates
+    dups = tiktoks['id'].duplicated(keep = 'last') #keep the most recent version of the tiktok
+    tiktoks = tiktoks[np.invert(dups)]
+    tiktoks = tiktoks.reset_index(drop = True)
+    
+    #remove rows with no video_title and no sound_transcribed
+    tiktoks = tiktoks[(tiktoks["sound_transcribed"] != 'NA') | (tiktoks["video_title"] != '')]
+    
+    #replace 'NA' in sound_transcribed with ''
+    tiktoks["sound_transcribed"] = [sound if sound != 'NA' else '' for sound in tiktoks["sound_transcribed"]]
+    tiktoks = tiktoks.reset_index(drop = True)
+    
+    #replace hashtag phrase with predicted phrase with spaces added
+    tiktoks['hashtags'] = [re.findall(r"#(\w+)",x) for x in tiktoks['video_title'] ] #create col for list of hashtag phrases
+    tiktoks['predicted_hashtag_words'] = [segment(' '.join(x)) for x in tiktoks['hashtags']] #create col for list of predicted phrases of each hashtag
+    tiktoks['predicted_hashtag_words'] = [' '.join(x) for x in tiktoks['predicted_hashtag_words']] #list -> string
+    tiktoks['original_video_title'] = tiktoks['video_title'] #keep original title in new column
+    tiktoks['video_title'] = [re.sub("#[A-Za-z0-9_]+","", x) for x in tiktoks['video_title']] #remove hashtagged phrases from title
+    tiktoks['video_title'] = tiktoks['video_title'] + tiktoks['predicted_hashtag_words'] #new title where hashtag phrases are replaced with their predicted words
+
+    #rank tiktoks in order of date pulled
+    tiktoks['date_pulled'] = pd.to_datetime(tiktoks.date_pulled)
+    tiktoks['date'] = tiktoks['date_pulled'].dt.date
+    tiktoks['rank'] = tiktoks['date'].rank(method='dense')
+    
+    return tiktoks
+
+
+def get_sentiment(tiktoks):
+    '''
+    A function that adds the sentiment analysis scores into columns in the TikTok dataframe
+    '''
+
+    # get sentiment analyzer
+    sid = SentimentIntensityAnalyzer()
+
+    #combine title and sound_transcribed
+    tiktoks['title_and_sound'] = tiktoks['video_title'].astype(str) + ' ' + tiktoks['sound_transcribed'].astype(str)
+
+    # get each score
+    tiktoks['Negative Sentiment'] = tiktoks['title_and_sound'].apply(lambda x: sid.polarity_scores(x)['neg'])
+    tiktoks['Neutral Sentiment'] = tiktoks['title_and_sound'].apply(lambda x: sid.polarity_scores(x)['neu'])
+    tiktoks['Positive Sentiment'] = tiktoks['title_and_sound'].apply(lambda x: sid.polarity_scores(x)['pos'])
+    tiktoks['Compound Sentiment'] = tiktoks['title_and_sound'].apply(lambda x: sid.polarity_scores(x)['compound'])
+    return tiktoks
+
+
+def make_scatterplot(tiktoks): 
+    """
+    Takes in a dataframe and returns a scatterplot of the sentiment analysis
+    """
+    #get start and end date
+    start = tiktoks['date'].min().strftime('%m/%d/%Y')
+    end = tiktoks['date'].max().strftime('%m/%d/%Y')
+    
+    # plot data
+    fig = px.scatter(tiktoks,
+                     x = 'Positive Sentiment',
+                     y = 'Negative Sentiment',
+                     color = 'Compound Sentiment',
+                     color_continuous_scale = ['rgb(255,0,80)', 'rgb(255,255,255)', 'rgb(0,242,234)'],
+                     range_color = (-1.1,1.1),
+                     template = 'plotly_dark',
+                     hover_name = 'original_video_title',
+                     hover_data = ['Neutral Sentiment', 'Compound Sentiment', 'like', 'sound_transcribed'],
+                     size = 'like',
+                     title = f'''Sentiment Analysis on Trending TikToks
+                     <br> <sup> TikToks from {start} - {end}</sup>''',
+                     size_max = 8,
+                     width = 800,
+                     height = 600)
+                     
+    fig.update_traces(marker_sizemin = 3)
+
+    fig.update_layout(
+        font={
+            'size': 14},
+        title = {
+            'x':0.1
+        })
+
+    return fig
+
+def sent_scores(tiktoks):
+    """
+    Takes in a dataframe, calculates the sentiment scores, 
+    Returns a dictionary containing the average sentiment score
+    """
+    ss = [SentimentIntensityAnalyzer().polarity_scores(text) for text in tiktoks['title_and_sound']]
+    add_dict = collections.Counter({'neg': 0.0, 'neu': 0.0, 'pos': 0.0, 'compound': 0.0})
+   
+    # add up all the sentiment scores of all the tiktoks
+    for s in ss:
+        counter = collections.Counter(s)
+        add_dict += counter
+    dict_ss = dict(add_dict)
+    
+    # take the average score
+    for k in dict_ss.keys():
+        dict_ss[k] = dict_ss[k]/len(ss)
+
+    return dict_ss
+
+def make_piechart(tiktoks):
+    """
+    Takes in a dataframe and returns a pie chart of the sentiment analysis
+    """
+    # get date range
+    start = tiktoks['date'].min().strftime('%m/%d/%Y')
+    end = tiktoks['date'].max().strftime('%m/%d/%Y')
+
+    dict_data = sent_scores(tiktoks).copy()
+    dict_data.pop('compound')
+    df_data = pd.DataFrame([{'sentiment': x, 'score': y} for x,y in dict_data.items()])
+    fig = px.pie(df_data, 
+                 names='sentiment', 
+                 values='score', 
+                 title = f'''Sentiment Analysis on Trending TikToks
+                     <br> <sup> TikToks from {start} - {end}</sup>''', 
+                 template = 'plotly_dark', 
+                 color_discrete_sequence = ['rgb(255,255,255)', 'rgb(0,242,234)', 'rgb(255,0,80)'],
+                 width = 800,
+                 height = 600)
+    return fig
+
+
+
+
+
